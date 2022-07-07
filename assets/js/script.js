ddb3d241a26ee0bdbb2c95d517d6fff5fecede68
LorenzoF <lorenzoferritsic@gmail.com>
2022-07-06 21:43:29 -0400

    Finished main card weather display

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,11 +31,8 @@ function searchHandle (e) {
         return cityInfo
     })
     .then (function (cityInfo) {
-        console.log(cityInfo.cityLat)
-        console.log(cityInfo.cityLon)
-        fetchWeather()
+        fetchWeather(cityInfo.cityLat, cityInfo.cityLon)
     })
-  
     // fetchWeather(data we received from fetchLocation)
   }
   
@@ -43,13 +40,13 @@ function searchHandle (e) {
   
   function fetchWeather (lat, lon) {
     // get some data for the weather in the location we searched - using an api call
-    var weatherSearchLink = ("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon=" + lon +"&appid=" + apiKey)
+    var weatherSearchLink = ("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon=" + lon + "&units=imperial&appid=" + apiKey)
     fetch(weatherSearchLink)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data)
+        renderWeather(data.daily)
     })
     // renderWeather(data)
   }
@@ -58,7 +55,28 @@ function searchHandle (e) {
   // FUNCTIONALITY FOR RENDERING CONTENT //
   
   function renderWeather(data) {
+    var card = document.getElementById("mainCard")
+    var mainTitle = document.getElementById("mainCardTitle")
+    var mainDesc = document.getElementById("mainCardWeather")
+    var mainImg = document.getElementById("mainCardImg")
+    var mainWind = document.getElementById("mainCardWind")
+    var mainUV = document.getElementById("mainCardUV")
+    var mainHumid = document.getElementById("mainCardHumidity")
+    var mainTemp = document.getElementById("mainCardTemp")
+    var Img = "http://openweathermap.org/img/wn/"
+    console.log(data)
+    var todaysWeather = data[0]
+    console.log(data[0])
+    mainTitle.textContent = cityInput.value
+    mainImg.src = Img + todaysWeather.weather[0].icon + "@4x.png"
+    mainDesc.textContent = todaysWeather.weather[0].description
+    mainTemp.textContent = todaysWeather.temp.day + " degrees farenheit"
+    mainWind.textContent = todaysWeather.wind_speed + "MPH"
+    mainUV.textContent = todaysWeather.uvi
+    mainHumid.textContent = todaysWeather.humidity + "% Humidity"
+    mainCard.style.display = "flex"
     // render the weather 
+
   }
   
   // HISTORY FEATURE //
